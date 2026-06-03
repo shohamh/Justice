a4b4d44c5a0f46873b0d38a7657bc4b7b50700ea
shohamh <shohamh@hotmail.com>
2026-06-03 17:19:31 +0300

    feat: add TelegramGate/AppGate to App.tsx, loginWithToken/refreshMe to AuthContext, placeholder pages

---
+++ frontend/src/pages/AdminInviteCodesPage.tsx
@@ -0,0 +1,3 @@
+export default function AdminInviteCodesPage() {
+  return <div>Admin Invite Codes</div>;
+}
---
+++ frontend/src/pages/TelegramSetupPage.tsx
@@ -0,0 +1,3 @@
+export default function TelegramSetupPage() {
+  return <div>Telegram Setup</div>;
+}
